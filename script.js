0bd86022ce4bb6ab0f71ab6980ad922e38885dae
Rahman Fazal <official.rahmanfazal@gmail.com>
2023-10-08 11:57:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+const accessKey = "7-gjKqordd4zMmZtfa3WGHSqhHlakz6atW4zEH6IIS0";
+
+
+const formElement = document.querySelector('form');
+const inputElement = document.getElementById('search-input');
+const searchResult = document.querySelector('.search-results');
+const showMore = document.getElementById('show-more-button');
+
+let inputData = "";
+let page =1;
+
+async function searchImages(){
+    inputData = inputElement.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=
+    ${inputData}&client_id=${accessKey}`
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results;
+    if(page === 1){
+        searchResult.innerHTML="";
+    }
+
+    results.map((result)=>{
+        const imageWrapper = document.createElement('div');
+        imageWrapper.classList.add('search-result');
+        const image = document.createElement('img');
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement('a');
+        imageLink.href=result.link;
+        imageLink.target ="_blank";
+        imageLink.textContent = result.alt_description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResult.appendChild(imageWrapper);
+    })
+    page++
+    if(page >  1){
+        showMore.style.display='block';
+
+    }
+}
+
+formElement.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    page = 1;
+
+    searchImages();
+})
+showMore.addEventListener("click", ()=>{
+    searchImages();
+})
